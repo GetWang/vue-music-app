c67ea99a65d16083ac96c69d76f07f799319819b
GetWang <wang_dresimap@qq.com>
2017-12-28 13:53:46 +0800

    初始化项目，增加图标字体和公共样式

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  #app
+    color: $color-theme
+
+</style>
