2bb80e7e5ede492620631b77e5581251b035d3a0
GetWang <wang_dresimap@qq.com>
2018-01-04 12:07:34 +0800

    用JSONP抓取歌手数据并规范化，开发listview组件之实现滚动列表

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,63 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="group in data" class="list-group">
+        <h2 class="list-group-title">{{ group.title }}</h2>
+        <ul>
+          <li class="list-group-item" v-for="item in group.items">
+            <img v-lazy="item.avatar" class="avatar">
+            <span class="name">{{ item.name }}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      Scroll
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        padding-left: 20px
+        line-height: 30px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          font-size: $font-size-medium
+          color: $color-text-l
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,90 @@
 <template>
-  <div>
-    singer
+  <div class="singer">
+    <list-view :data="singerList"></list-view>
   </div>
 </template>
 
 
 <script type="text/ecmascript-6">
-  export default {}
+  import { getSingerList } from 'api/singer'
+  import { ERR_OK } from 'api/config'
+  import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
+  export default {
+    data() {
+      return {
+        singerList: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singerList = this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 为了得到有序列表，我们需要处理 map 对象，得到一个有序数组
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {         // 对以字母进行分类的数组ret进行升序排列
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      }
+    },
+    components: {
+      ListView
+    }
+  }
 </script>
 
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
